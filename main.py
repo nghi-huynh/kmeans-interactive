94d010fc2bd3e1913cff963ed008716693c4c010
Mathias Gout <mathias.gout@gmail.com>
2020-09-11 12:00:02 +0200

    init main

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import numpy as np
+import tkinter as tk
+
+class KmeansInteractive:
+    
+    def __init__(self):
+        
+        self.master = tk.Tk()
+        
+        # Window customization
+        self.master.title("Interactive K-means")
+
+        self.master.mainloop()
+
+
+if __name__ == "__main__":
+    KmeansInteractive()
